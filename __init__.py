454cd93d89402422c09d4ee60b8310dd4487fd80
colonelkrud <admin@colonelkrud.com>
2019-02-21 00:12:55 -0500

    fix classes and pass self

--- __init__.py
+++ __init__.py
@@ -4,44 +4,45 @@ from statistics import mode, StatisticsError
 import random
 import itertools
 
-DICE_ROLL_RESPONSES = [
-    "The dice says {number}",
-    "You rolled a {number}",
-    "It's a {number}"
-]
+class RandomSkill(Skill):
+
+    def __init__(self, opsdroid, config):
+        self.self.DICE_ROLL_RESPONSES = [
+            "The dice says {number}",
+            "You rolled a {number}",
+            "It's a {number}"
+            ]
+        self.self.COIN_FLIP_RESPONSES = [
+            "It landed on {result}",
+            "You got {result}",
+            "It's {result}"
+        ]
+
+        self.MODE_EXISTS = """
+            <p>You rolled the following: {list}</p>
+            <p>The average is {average} The mode is {mode}</p>
+            <p>The minimum value was: {min} and the maximum value was: {max}</p>
+            """
+        self.MODE_DNE = """
+            <p>You rolled the following: {list}</p>
+            <p>The average is {average} There is no unique mode</p>
+            <p>The minimum value was: {min} and the maximum value was: {max}</p>
+            """
 
 
-COIN_FLIP_RESPONSES = [
-    "It landed on {result}",
-    "You got {result}",
-    "It's {result}"
-]
-
-MODE_EXISTS = """
-    <p>You rolled the following: {list}</p>
-    <p>The average is {average} The mode is {mode}</p>
-    <p>The minimum value was: {min} and the maximum value was: {max}</p>
-    """
-MODE_DNE = """
-    <p>You rolled the following: {list}</p>
-    <p>The average is {average} There is no unique mode</p>
-    <p>The minimum value was: {min} and the maximum value was: {max}</p>
-    """
-
-class RandomSkill(skill):
     @match_regex(r'roll a dice', case_sensitive=False)
-    async def roll_a_dice(opsdroid, config, message):
+    async def roll_a_dice(self, opsdroid, config, message):
         """
         Rolls a single dice and replies with the result.
         Usage: roll a dice
         """
         number = random.randint(1, 6)
-        text = random.choice(DICE_ROLL_RESPONSES).format(number=number)
+        text = random.choice(self.self.DICE_ROLL_RESPONSES).format(number=number)
         await message.respond(text)
 
 
     @match_regex(r'roll a(n)? ([1-9][0-9]*) sided dice$', case_sensitive=False)
-    async def roll_a_n_sided_dice(opsdroid, config, message):
+    async def roll_a_n_sided_dice(self, opsdroid, config, message):
         """
         Rolls a single dice of given sides and replies with the result.
         Usage: roll a(n) (#sides) sided dice
@@ -49,17 +50,17 @@ class RandomSkill(skill):
         sides = int(message.regex.group(2))
         if sides == 2:
             result = random.choice(["heads", "tails"])
-            text = random.choice(COIN_FLIP_RESPONSES).format(result=result)
+            text = random.choice(self.COIN_FLIP_RESPONSES).format(result=result)
             await message.respond(text)
         else:
             number = random.randint(1, sides)
-            text = random.choice(DICE_ROLL_RESPONSES).format(number=number)
+            text = random.choice(self.DICE_ROLL_RESPONSES).format(number=number)
             await message.respond(text)
 
 
     @match_regex(r'roll ([1-9][0-9]*) ([1-9][0-9]*) sided dice$',
                 case_sensitive=False)
-    async def roll_many_dice(opsdroid, config, message):
+    async def roll_many_dice(self, opsdroid, config, message):
         """
         Rolls a given number of dice of given sides and replies with the results.
         Also gives some basic stats about the dice rolled.
@@ -70,7 +71,7 @@ class RandomSkill(skill):
         if sides == 2 and 1 <= dice <= 1024:
             await message.respond("You only get one.")
             result = random.choice(["heads", "tails"])
-            text = random.choice(COIN_FLIP_RESPONSES).format(result=result)
+            text = random.choice(self.COIN_FLIP_RESPONSES).format(result=result)
             await message.respond(text)
         elif 1 <= dice <= 1024 and sides != 2:
             res = []
@@ -94,7 +95,7 @@ class RandomSkill(skill):
 
 
     @match_regex(r'roll a(n)? d([1-9][0-9]*)$', case_sensitive=False)
-    async def roll_dx(opsdroid, config, message):
+    async def roll_dx(self, opsdroid, config, message):
         """
         Rolls a dice of given sides and replies with the results.
         Usage: roll a(n) d(#sides)
@@ -102,16 +103,16 @@ class RandomSkill(skill):
         sides = int(message.regex.group(2))
         if sides == 2:
             result = random.choice(["heads", "tails"])
-            text = random.choice(COIN_FLIP_RESPONSES).format(result=result)
+            text = random.choice(self.COIN_FLIP_RESPONSES).format(result=result)
             await message.respond(text)
         else:
             number = random.randint(1, sides)
-            text = random.choice(DICE_ROLL_RESPONSES).format(number=number)
+            text = random.choice(self.DICE_ROLL_RESPONSES).format(number=number)
             await message.respond(text)
 
 
     @match_regex(r'roll ([1-9][0-9]*) d([1-9][0-9]*)$', case_sensitive=False)
-    async def roll_many_dx(opsdroid, config, message):
+    async def roll_many_dx(self, opsdroid, config, message):
         """
         Rolls a given number of dice of given sides and replies with the results.
         Also gives some basic stats about the dice rolled.
@@ -122,7 +123,7 @@ class RandomSkill(skill):
         if sides == 2 and 1 <= dice <= 1024:
             await message.respond("You only get one.")
             result = random.choice(["heads", "tails"])
-            text = random.choice(COIN_FLIP_RESPONSES).format(result=result)
+            text = random.choice(self.COIN_FLIP_RESPONSES).format(result=result)
             await message.respond(text)
         elif 1 <= dice <= 1024 and sides != 2:
             res = []
@@ -134,11 +135,11 @@ class RandomSkill(skill):
             rolls = str(res).strip('[]')
             try:
                 unique_mode = str(mode(res))
-                await message.respond(MODE_EXISTS.format(list=rolls,
+                await message.respond(self.MODE_EXISTS.format(list=rolls,
                                     average=average, mode=unique_mode,
                                     min=minimum_roll, max=maximum_roll))
             except StatisticsError:
-                await message.respond(MODE_DNE.format(list=rolls, average=average,
+                await message.respond(self.MODE_DNE.format(list=rolls, average=average,
                                                     min=minimum_roll,
                                                     max=maximum_roll))
         else:
@@ -146,11 +147,11 @@ class RandomSkill(skill):
 
 
     @match_regex(r'flip a coin', case_sensitive=False)
-    async def flip_a_coin(opsdroid, config, message):
+    async def flip_a_coin(self, opsdroid, config, message):
         """
         Replies with heads or tails.
         Usage: flip a coin
         """
         result = random.choice(["heads", "tails"])
-        text = random.choice(COIN_FLIP_RESPONSES).format(result=result)
+        text = random.choice(self.COIN_FLIP_RESPONSES).format(result=result)
         await message.respond(text)
